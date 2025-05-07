96f1e69f0aba4f6704367fd583701963dc92cf7c
Cyberfn <fernandononatocco@gmail.com>
2025-05-07 10:54:08 -0300

    Ajusta rota de login

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => response()->view('home'));
 Route::get('/api/auth-check', [AuthController::class, 'authCheck']);
+Route::get('/login', fn () => response()->view('login'))->middleware('guest');
 Route::post('/login', [AuthController::class, 'login']);
+
 
 Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/perfil', fn () => response()->view('perfil'));
 });
